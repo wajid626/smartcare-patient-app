c791880a4695ae38393f84af36dfbbee5a68c9da
wajid626 <wajid626@gmail.com>
2015-03-08 14:19:26 -0700

    Integrated myappointments with restful service

--- src/com/smartcare/bookings/activities/MyAppointmentsActivity.java
+++ src/com/smartcare/bookings/activities/MyAppointmentsActivity.java
@@ -1,9 +1,18 @@
 package com.smartcare.bookings.activities;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,17 +23,20 @@ import android.widget.ListView;
 import com.smartcare.bookings.R;
 import com.smartcare.bookings.Appointment;
 import com.smartcare.bookings.SmartCareBookingsApplication;
+import com.smartcare.bookings.rest.RestClient;
 
 public class MyAppointmentsActivity extends ActivityBase {
 	private final Context context = this;
 	ListView lstAppointments;
 	
-	Appointment[] appointments;
+	//Appointment[] appointments;
+	List<String> appointments;
 	
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        appointments = ((SmartCareBookingsApplication)getApplication()).getBookedAppointments();
+        //appointments = ((SmartCareBookingsApplication)getApplication()).getBookedAppointments();
+       appointments = getAppointments("Wajid");
 
         setContentView(R.layout.activity_my_appointments);
         super.onCreate(savedInstanceState);
@@ -38,7 +50,7 @@ public class MyAppointmentsActivity extends ActivityBase {
     
     protected void setUiComponents () {
     	lstAppointments = (ListView) findViewById(R.id.lstAppointments);
-    	fillListAppointments(appointments);
+    	fillListAppointmentsNew(appointments);
     }
     
     protected void setUiEventHandlers () {
@@ -62,8 +74,64 @@ public class MyAppointmentsActivity extends ActivityBase {
 		// See Appointment.toString()
 		ArrayAdapter<Appointment> adapter = new ArrayAdapter<Appointment>(context,
 	  		  android.R.layout.simple_list_item_1, android.R.id.text1, appointments);
-	  
 		lstAppointments.setAdapter(adapter);
-		
 	}
+ 
+    private void fillListAppointmentsNew (List<String> appointments) {
+		// The ArrayAdapter calls .toString() on whatever is passed to it
+		// See Appointment.toString()
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(context,
+	  		  android.R.layout.simple_list_item_1, android.R.id.text1, appointments);
+		lstAppointments.setAdapter(adapter);
+	}
+
+    
+    private List<String> getAppointments(String patientName) {
+    	RestClient rsClient = new RestClient("http://smartcare-services.elasticbeanstalk.com/rest/AdminService/getAppointmentDetails");
+    	List<String> slots = new ArrayList<String>();
+    	rsClient.addParam("patientName", "wajid");
+    	try {
+			rsClient.execute(RestClient.RequestMethod.GET);
+			System.out.println("-------------");
+			System.out.println(rsClient.getErrorMessage());
+			System.out.println(rsClient.getResponseCode());
+			System.out.println(rsClient.getResponse());
+			String responseArray =rsClient.getResponse();
+			 Log.i("Raje-Restclient", rsClient.getResponse().toString());
+			 Appointment[] app = new Appointment[10];
+			 
+			 
+			 JSONArray jArray = new JSONArray(rsClient.getResponse().toString());
+			 boolean flag=true;
+			 
+			 for (int i=0; i < jArray.length() && flag; i++)
+			 {
+			     try {
+			    	 StringBuffer s = new StringBuffer("Appontment with ");
+						
+			    	 //Parsing the Json array from the service response
+			    	 JSONObject json_data = jArray.getJSONObject(i);
+			    	  String physicianName = json_data.getString("PhysicianName");
+			          String appointmentDateTime =  json_data.getString("AppointmentDateTime");
+			          s.append(physicianName);
+			          s.append(" on ");
+			          s.append(appointmentDateTime);
+			          s.append(" at ");
+			          s.append("San Jose");
+			          //app[i] = new Appointment(null, null, appointmentDateTime);
+			          slots.add(s.toString());
+			     } catch (JSONException e) {
+			         // Oops
+			     }
+			 }
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Exception occurred");
+			e.printStackTrace();
+		}
+       // return new PayPalPayment(new BigDecimal("10.75"), "USD", "My medical charges",
+       //         paymentIntent);
+	return slots;
+    }
 }
